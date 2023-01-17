bf7cbdd0bcd2c13d9abf320e0ec353650bddd2e1
chandrashekhar-meshram <107882415+chandrashekhar-meshram@users.noreply.github.com>
2023-01-17 23:06:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lession5-vanilaJS-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-s7xykl)
---
+++ script.js
@@ -0,0 +1,9 @@
+
+// how to use prompt, alert and console
+//let userName = prompt('Your user name');
+
+//console.log('Hello! ' + userName);
+//prompt("hi");
+//alert("hi "+ userName);
+
+
